fbf402126a3836ce2cd30a872e850d1b15496cc9
jamesg <jamesthefierce@gmail.com>
2016-10-21 18:58:52 +0000

    working on coffe shop sales week

--- coffee-shop-math.js
+++ coffee-shop-math.js
@@ -1,30 +1,36 @@
-function calculateWeeklySales(sun, mon, tue, wed, thr, fri, sat) {
-        console.log(sun + mon + tue + wed + thr + fri + sat);
+function calculateWeeklySales(SalesWeek) {
+        console.log(SalesWeek.sunday
+        + SalesWeek.monday 
+        + SalesWeek.tuesday 
+        + SalesWeek.wednesday 
+        + SalesWeek.thursday 
+        + SalesWeek.friday 
+        + SalesWeek.saturday);
     
+
 }
+var SalesWeek = {
+
+    sunday: 150,
+
+    monday: 134,
+
+    tuesday: 288,
+
+    wednesday: 272,
+
+    thursday: 331,
+
+    friday: 392,
+    
+    saturday: 405,
+};
 
 
-
-
-var sunday = 150;
-
-var monday = 134;
-
-var tuesday = 288;
-
-var wednesday = 272;
-
-var thursday = 331;
-
-var friday = 392;
-
-var saturday = 405;
-
-console.log(sunday + monday + tuesday + wednesday + thursday + friday + saturday);
 
 
 // call function to calculate weekly sales
-calculateWeeklySales(sunday, monday, tuesday, wednesday, thursday, friday, saturday);
+calculateWeeklySales(SalesWeek);
 
 
 var a = 1;
